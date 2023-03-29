dc6e89fb7ad3a04d951379a6a97add14e00922d1
Ayush Singh <94789217+singhayush1260@users.noreply.github.com>
2023-03-30 01:55:34 +0530

    fixed blogPost bugs

--- pages/blog/[postSlug].js
+++ pages/blog/[postSlug].js
@@ -3,14 +3,18 @@ import classes from '../../styles/pages/BlogPost.module.scss';
 import sanityClient from '../../sanity/sanity';
 import dateFormat from 'dateformat';
 const BlogPost=({post})=>{
-
+    if(!post){
+       return(
+        <div className={classes.page_wrapper}></div>
+       )
+    }
     return(
         <div className={classes.page_wrapper}>
             <div className={classes.container}>
                <div className={classes.post_metaData}>
-               <h1>{post.title?post.title:"Blog Post Title"}</h1>
-               <p>{post.author?post.author:"Author Name"}</p>
-               <p>{post.publishedAt ? dateFormat(post.publishedAt, "mmmm dS, yyyy"):"Published Date"}</p>
+               <h1>{post.title?post.title:"Some Post Title"}</h1>
+               <p>{post.author?post.author:"Anonymous"}</p>
+               <p>{post.publishedAt?dateFormat(post.publishedAt, "mmmm dS, yyyy"):"Published Date"}</p>
                <p className={classes.social_share}> 
                </p>
                </div>
@@ -31,7 +35,7 @@ export async function getStaticPaths() {
     const paths = slugs.map((slug) => {
       return {
         params: {
-          postSlug: `${slug.slug.current}`,
+          postSlug: `${slug.slug?slug.slug.current:""}`,
         },
       };
     });
@@ -40,14 +44,16 @@ export async function getStaticPaths() {
       paths: paths,
     };
   }
-
+  
   export async function getStaticProps(context) {
     const slug = context.params.postSlug;
-    const post = await sanityClient.fetch(`*[slug.current=="${slug}"]{
-      title, _id,slug,mainImage{
-        asset->{_id, url}},body,"author":author->name,categories,publishedAt }`);
-    return {
-      props: { post: post[0] },
-    };
+    let post = await sanityClient.fetch(`*[slug.current=="${slug}"]{title,_id,slug,
+    mainImage{asset->{ _id, url}},body,"author":author->name,categories,publishedAt }`);
+    const notFound = post[0] ? false : true;
+    
+  
+   return { props: { post: post[0] },notFound};
   }
+
+
 export default BlogPost;
