df2547e944d9d8d1d592f5263b669037795810de
Ayush Singh <singh.dev6537@gmail.com>
2023-03-30 00:32:13 +0530

    made blog post dynamic

--- pages/blog/[postSlug].js
+++ pages/blog/[postSlug].js
@@ -1,35 +1,53 @@
 
 import classes from '../../styles/pages/BlogPost.module.scss';
-import { FacebookShareButton, TwitterShareButton } from 'react-share';
-import {CiFacebook, CiTwitter} from 'react-icons/ci';
-const BlogPost=()=>{
-
-//const currentURL=window.location.href;
+import sanityClient from '../../sanity/sanity';
+import dateFormat from 'dateformat';
+const BlogPost=({post})=>{
 
     return(
         <div className={classes.page_wrapper}>
             <div className={classes.container}>
                <div className={classes.post_metaData}>
-               <h1>How to use 3B pencil?</h1>
-               <p>Ayush Singh</p>
-               <p>12th March, 2023</p>
+               <h1>{post.title?post.title:"Blog Post Title"}</h1>
+               <p>{post.author?post.author:"Author Name"}</p>
+               <p>{post.publishedAt ? dateFormat(post.publishedAt, "mmmm dS, yyyy"):"Published Date"}</p>
                <p className={classes.social_share}> 
-               <FacebookShareButton url={""}><CiFacebook/></FacebookShareButton>   
-               <TwitterShareButton  url={""}><CiTwitter/></TwitterShareButton>
                </p>
                </div>
                 <div className={classes.post_body}>
-                <img src='/images/utils/image_placeholder.jpg'/>
+                <img src={post.mainImage?post.mainImage.asset.url:'/images/utils/image_placeholder.jpg'} alt="thumbnail" />
                 <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur animi numquam rerum ea,
-                 commodi aliquid beatae deserunt repudiandae nobis iure sunt exercitationem quaerat dolor
-                  eos? Iusto quas maxime ducimus atque. Veritatis nostrum a tenetur dolores quidem, omnis 
-                  consectetur cupiditate. Possimus rerum eos magni nostrum reprehenderit voluptas vel aliquam
-                   dolores atque.
+                {post.body[0].children[0].text}
                 </p>
                 </div>               
             </div>
         </div>
     )
 }
+
+export async function getStaticPaths() {
+    const slugs = await sanityClient.fetch(`*[_type=='post']{
+          slug}`);
+    const paths = slugs.map((slug) => {
+      return {
+        params: {
+          postSlug: `${slug.slug.current}`,
+        },
+      };
+    });
+    return {
+      fallback: true,
+      paths: paths,
+    };
+  }
+
+  export async function getStaticProps(context) {
+    const slug = context.params.postSlug;
+    const post = await sanityClient.fetch(`*[slug.current=="${slug}"]{
+      title, _id,slug,mainImage{
+        asset->{_id, url}},body,"author":author->name,categories,publishedAt }`);
+    return {
+      props: { post: post[0] },
+    };
+  }
 export default BlogPost;
